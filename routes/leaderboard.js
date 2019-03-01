d7c4ef0c7cbcd36b7f493ba438f10aa4ef6ce54f
Archit Kaul <architkl@gmail.com>
2019-03-01 22:08:32 +0530

    Rank Assigned

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -4,28 +4,21 @@ module.exports = () => {
 	let exp = {};
 
 	exp.getLeaderboard = (req, res) => {
-		db.query('SELECT uid, username, score FROM Users where access = 0 ORDER BY score DESC', (err, users) => {
-			if (err) {
-				console.log(err);
-				return;
-			}
-
-			// console.log(users);
-
-			db.query('SELECT COUNT(*) as units, uid from Grid group by uid', (err, result) => {
+		db.query('SELECT u.uid, u.username, u.score, count(*) as health \
+			FROM Users u INNER JOIN Grid g ON u.uid = g.uid \
+			WHERE g.isactive = 1 \
+			GROUP BY u.uid \
+			ORDER BY u.score DESC', (err, users) => {
 				if (err) {
 					console.log(err);
 					return;
 				}
 
-				// console.log(result);
-
-				for (var i in result) {
-					users[i].health = result[i].units;
+				for (var i in users) {
+					users[i].rank = parseInt(i) + 1;
 				}
 
 				res.send(users);
-			});
 		});
 	}
 
