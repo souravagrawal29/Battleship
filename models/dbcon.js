36d64e987b1c792002f0f07b7f77cf8b4db52cc1
Akshat Tulsani <akshat.tulsani@elucidata.io>
2019-03-05 20:16:20 +0530

    dbcon.js  error removed

--- models/dbcon.js
+++ models/dbcon.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'kt97tulsani',
-    database: 'battleship19'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection;
