0a755f6ded887f89da3bef716b4818456950f905
Archit Kaul <architkl@gmail.com>
2019-03-12 21:21:23 +0530

    Leaderboard Fixed

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -8,7 +8,7 @@ module.exports = () => {
 			FROM Users u INNER JOIN Grid g ON u.uid = g.uid \
 			WHERE g.isactive = 1 \
 			GROUP BY u.uid \
-			ORDER BY u.score DESC', (err, users) => {
+			ORDER BY u.score DESC, health DESC', (err, users) => {
 				if (err) {
 					console.log(err);
 					return;
@@ -17,6 +17,8 @@ module.exports = () => {
 				for (var i in users) {
 					users[i].rank = parseInt(i) + 1;
 				}
+
+				console.log(users);
 
 				res.render('leaderboard', {users: users.map(user => ({...user}))});
 		});
